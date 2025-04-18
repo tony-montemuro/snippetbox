12983565b462d6c5b8aaf984c946b6abfcf6ce9f
tony.montemuro <tonyamontemuro@gmail.com>
2025-04-17 21:32:02 -0500

    lesson 10.2

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -2,4 +2,8 @@ package models
 
 import "errors"
 
-var ErrNoRecord = errors.New("models: no matching records found")
+var (
+	ErrNoRecord           = errors.New("models: no matching records found")
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+	ErrDuplicateEmail     = errors.New("models: duplicate email")
+)
